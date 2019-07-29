09019b580b5b4399795c9650364745adb614023e
MatthisPralat <matthispralat@gmail.com>
2019-07-29 23:33:17 +0200

    Modification du bat pour blender
    Signed-off-by: MatthisPralat <matthispralat@gmail.com>

--- BatBlender/Blender.bat
+++ BatBlender/Blender.bat
@@ -1,3 +1,3 @@
-cd "E:\Users\user\Desktop\blender-2.80rc1-windows64"
-
-blender "E:\Users\user\Desktop\BarBlender\bat.blend" --python "E:\Users\user\Desktop\BarBlender\pythonscript.py" 
+cd "Votre Chemin pour blender"
+blender "Le chemin de votre ficher blender \bat.blend" --python "Le chemin de votre fichier python \pythonscript.py" 
+pause
