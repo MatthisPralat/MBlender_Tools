ce2485cc259da13bd074eb029d73b43cd9655f2f
MatthisPralat <matthispralat@gmail.com>
2019-08-11 16:06:11 +0200

    Pep8 Pour MaterialAsignementUtils.py
    Signed-off-by: MatthisPralat <matthispralat@gmail.com>

--- MaterialAsignementUtils.py
+++ MaterialAsignementUtils.py
@@ -1,60 +1,67 @@
 import bpy
 
-AllMaterials = bpy.data.materials   # je choppe les materieux
-AllMat = [ (mat.name, mat.name, mat.name) for mat in AllMaterials ] # je les mets dans un Enum
-MatToReplace = AllMat   # Pour remplacer celui-ci
-MatReplace = AllMat     # Par Celui ci 
+# je choppe les materieux
+AllMaterials = bpy.data.materials
+# je les mets dans un Enum
+AllMat = [(mat.name, mat.name, mat.name) for mat in AllMaterials]
+# Pour remplacer celui-ci
+MatToReplace = AllMat
+# Par Celui ci
+MatReplace = AllMat
 
 # C'est ici que j'affiche mes variables dynamiques.
-#---------------------------------------------------------------------------------------------------
-# CLASS backfacemat 
+# -----------------------------------------------------------------------------
+# CLASS backfacemat
 #       Remplace les materials
-#---------------------------------------------------------------------------------------------------
-class MaterialUtilitiesPropertyGroup( bpy.types.PropertyGroup ):
+# -----------------------------------------------------------------------------
 
-    MaterialToReplace = bpy.props.EnumProperty( items= MatToReplace )
-    MaterialReplace = bpy.props.EnumProperty( items= MatReplace )
 
-    BackfaceBool = bpy.props.BoolProperty( 
-        name = "BackfaceBool",
-        description = " Backface all materials",
-        default = False
+class MaterialUtilitiesPropertyGroup(bpy.types.PropertyGroup):
+    MaterialToReplace = bpy.props.EnumProperty(items=MatToReplace)
+    MaterialReplace = bpy.props.EnumProperty(items=MatReplace)
+    BackfaceBool = bpy.props.BoolProperty(
+        name="BackfaceBool",
+        description="Backface all materials",
+        default=False
     )
-   
+
+
 def MATERIAL_REPLACE(mtr, mr):
 
     objects = bpy.context.scene.objects
-    
+
     for obj in objects:
         if obj.type == "MESH":
             i = -1
-            for mat in obj.data.materials: 
+            for mat in obj.data.materials:
                 i = i + 1
                 if mat.name == str(mtr):
                     obj.data.materials[i] = bpy.data.materials[str(mr)]
-                    
 
-#---------------------------------------------------------------------------------------------------
-# CLASS backfacemat 
+# -----------------------------------------------------------------------------
+# CLASS backfacemat
 #       Active/desactive les backfaces sur tout les materials
-#---------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+
 class SimpleOperator(bpy.types.Operator):
- 
+
     """Tooltip"""
     bl_idname = "object.simple_operator"
     bl_label = "Remplace material"
 
     def execute(self, context):
-        
-        props = context.scene.MaterialUtilitiesPropertyGroup  
-        MATERIAL_REPLACE( props.MaterialToReplace, props.MaterialReplace )
+        props = context.scene.MaterialUtilitiesPropertyGroup
+        MATERIAL_REPLACE(props.MaterialToReplace, props.MaterialReplace)
         return {'FINISHED'}
 
 
-#---------------------------------------------------------------------------------------------------
-# CLASS backfacemat 
+# -----------------------------------------------------------------------------
+# CLASS backfacemat
 #       Active/desactive les backfaces sur tout les materials
-#---------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+
 class BackfaceMaterial(bpy.types.Operator):
 
     bl_idname = "object.backfacematerial_operator"
@@ -68,21 +75,20 @@ class BackfaceMaterial(bpy.types.Operator):
         print(props.BackfaceBool)
 
         i = -1
-        for num in bpy.data.materials :
+        for num in bpy.data.materials:
             i = i + 1
             print(i)
-            print(num) 
+            print(num)
             print(bpy.data.materials[i])
             bpy.data.materials[i].use_backface_culling = BackfaceBoolVal
             print(bpy.data.materials[i].use_backface_culling)
 
+        return{'FINISHED'}
 
-        return {'FINISHED'}
 
-
-#---------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # CLASS  l'Interface
-#---------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 class MU_PT_pane(bpy.types.Panel):
 
     bl_space_type = 'VIEW_3D'
@@ -91,41 +97,45 @@ class MU_PT_pane(bpy.types.Panel):
     bl_category = "Tool"
 
     def draw(self, context):
-        
+
         layout = self.layout
         obj = context.object
-       
+
         props = context.scene.MaterialUtilitiesPropertyGroup
 
         row = layout.row()
         layout.label(text="Material Replace")
-       
-        #--------------------------------------------------------
+
+        # --------------------------------------------------------
         # Mes 2 magnifiques boutons !
-        #--------------------------------------------------------
+        # --------------------------------------------------------
         row = layout.row()
         row.prop(props, "MaterialReplace")
-       
+
         row = layout.row()
         row.prop(props, "MaterialToReplace")
 
-        #--------------------------------------------------------
+        # --------------------------------------------------------
         # Mon magnifique apply !
-        #--------------------------------------------------------
-        layout.label(text="Replace by this Material in all object")   
+        # --------------------------------------------------------
+        layout.label(text="Replace by this Material in all object")
         row = layout.row()
         row.operator('object.simple_operator')
 
         row = layout.row()
-        layout.label(text="BackeFace") 
+        layout.label(text="BackeFace")
         row.prop(props, "BackfaceBool")
 
         row = layout.row()
-        layout.label(text="Apply backface to all materials")   
+        layout.label(text="Apply backface to all materials")
         row.operator("object.backfacematerial_operator")
 
 
-classes = [SimpleOperator, MU_PT_pane, MaterialUtilitiesPropertyGroup, BackfaceMaterial ]
+classes = [
+    SimpleOperator,
+    MU_PT_pane,
+    MaterialUtilitiesPropertyGroup,
+    BackfaceMaterial]
 
 
 def register():
@@ -133,6 +143,7 @@ def register():
         bpy.utils.register_class(cls)
     bpy.types.Scene.MaterialUtilitiesPropertyGroup = bpy.props.PointerProperty(
         type=MaterialUtilitiesPropertyGroup)
+
 
 def unregister():
     for cls in classes:
