ed8dbb18a611f332817c186279a55de93eac80eb
Matthis Pralat <matthispralat@gmail.com>
2019-09-17 14:07:47 +0200

    Group Selection update
    Signed-off-by: Matthis Pralat <matthispralat@gmail.com>

--- GroupSelection.py
+++ GroupSelection.py
@@ -6,6 +6,13 @@ i = 0  # number of objects
 Null = 0
 
 
+def Unparent():
+    ''' Just unparent sel for escape errors'''
+    for obj in SelObj:
+        obj2 = obj
+        bpy.context.view_layer.objects.active = obj
+        bpy.ops.object.parent_clear(type='CLEAR_KEEP_TRANSFORM')
+
 def Location():
     ''' Add the Location of each selected objects'''
 
@@ -15,7 +22,6 @@ def Location():
     for obj in SelObj:
         i = i + 1
         X, Y, Z = X + obj.matrix_world.to_translation().x, Y + obj.matrix_world.to_translation().y, Z + obj.matrix_world.to_translation().z
-        print("Adition")
         print(X, Y, Z)
 
 
@@ -39,11 +45,8 @@ def ParentToNull():
         obj.matrix_parent_inverse = null.matrix_world.inverted()
 
 
-Location()
-average()
-CreateNull()
-ParentToNull()
-
-bpy.data.window_managers[0].keyconfigs.active.keymaps['Mesh'].keymap_items.new('op.idname', value='PRESS', type='A',
-                                                                               ctrl=True, alt=True, shift=True,
-                                                                               oskey=True)
+Unparent() # unparent, selected objects
+Location() # get the location of objects
+average()  # Add and divide the location by the number of objects
+CreateNull() # create a null to the average position
+ParentToNull() # Parent the objects in the new null object
